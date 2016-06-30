269c8628f9648c1e14173b87be08db1e93d55e96
alejandrina1 <league@Mac-mini-3.local>
2016-06-29 19:02:08 -0700

    6/29/16

---
+++ Lion.java
@@ -0,0 +1,20 @@
+
+public class Lion {
+String gender;
+String size;
+int manesize;
+void roar(){
+System.out.println("ROAR!");	
+}
+void run(){
+System.out.println("*Run*");	
+}
+Lion(String gender,String size,int manesize){
+	this.gender = gender;
+	this.size = size;
+	this.manesize = manesize;
+	
+	
+}
+}
+
---
+++ LionRunner.java
@@ -0,0 +1,6 @@
+
+public class LionRunner {
+public static void main(String[] args) {
+	Lion jerry = new Lion ("Male",)
+}
+}
